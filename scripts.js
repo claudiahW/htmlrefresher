1be638e80710850965a25bc121c9606b76405977
claudiahW <murungiclaud@gmail.com>
2024-01-14 16:05:26 +0300

    Add get location function

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const x = document.getElementById("demo");
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+  
+  
